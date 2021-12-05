64f2bb0dbb1ee57dc903d91fb76d025ce9aa4214
Mikhail Stankevich <Mikhail_Stankevich@epam.com>
2021-12-05 15:18:18 +0300

    feat: create users rest api

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,40 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const DB = require('../../database');
+
+const getAll = async () => DB.users;
+
+const getById = async (id) => DB.users.find(u => u.id === id);
+
+const create = async (user) => {
+  DB.users.push(user);
+  return user;
 };
 
-module.exports = { getAll };
+const update = async (id, body) => {
+  const userIndex = DB.users.findIndex(u => u.id === id);
+
+  if (userIndex === -1) {
+    return null;
+  }
+
+  DB.users[userIndex] = { ...DB.users[userIndex], ...body };
+  return DB.users[userIndex];
+};
+
+const remove = async (id) => {
+  const userIndex = DB.users.findIndex(u => u.id === id);
+
+  if (userIndex === -1) {
+    return null;
+  }
+
+  DB.tasks = DB.tasks.map(task => {
+    if(task.userId === id) {
+      return {...task, userId: null };
+    }
+    return task;
+  })
+
+  return DB.users.splice(userIndex, 1);
+};
+
+module.exports = { getAll, getById, create, update, remove };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,37 @@
 const usersRepo = require('./user.memory.repository');
 
+
+const validateUserBody = (req, res, next) => {
+  const { name, login, password } = req.body || {};
+
+  if (!password) {
+    return res.status(400).json({ message: 'User password required' });
+  }
+  if (!name) {
+    return res.status(400).json({ message: 'User name required' });
+  }
+  if (!login) {
+    return res.status(400).json({ message: 'User login required' });
+  }
+  next();
+  return null;
+};
+
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getUserById = (id) => usersRepo.getById(id);
+
+const createUser = (user) => usersRepo.create(user);
+
+const updateUser = (id, body) => usersRepo.update(id, body);
+
+const deleteUser = (id) => usersRepo.remove(id);
+
+module.exports = {
+  getAll,
+  createUser,
+  getUserById,
+  updateUser,
+  deleteUser,
+  validateUserBody,
+};
